5b46f465f24fc83e67255a0f6ba3cd34f2d9d385
Alan <alanbiegun13@gmail.com>
2020-04-12 15:58:41 +0200

    Uproszczenie

--- WSTI.WP.Kompozyt/Components/TextChapter.cs
+++ WSTI.WP.Kompozyt/Components/TextChapter.cs
@@ -1,37 +1,17 @@
 ﻿using System;
-using System.Collections.Generic;
 using WSTI.WP.Kompozyt.BaseClasses;
 
 namespace WSTI.WP.Kompozyt.Components
 {
 	public class TextChapter : BaseChapter
 	{
-		public List<string> Paragraphs { get; set; }
-
 		public TextChapter(string title) : base(title)
 		{
-			Paragraphs = new List<string>();
 		}
-		public TextChapter(string title, List<string> newParagraphs) : base(title)
-		{
-			Paragraphs = new List<string>();
-			Paragraphs.AddRange(newParagraphs);
-		}
-
-		public void AddParagraph(string text)
-		{
-			Paragraphs.Add(text);
-		}
-
+		
 		public override void PrintTitle()
 		{
 			Console.WriteLine(Title);
-		}
-
-		public void PrintParagraphs()
-		{
-			foreach (var paragraph in Paragraphs)
-				Console.WriteLine($"\t{paragraph}");
 		}
 	}
 }
